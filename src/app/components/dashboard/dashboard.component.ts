33092e24215e72a61d0c3c07b44f8b4701e144b4
Moses <moses.m917@gmail.com>
2024-03-15 12:03:20 +0200

    dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UserInfoService } from 'src/app/services/user-info.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  user: any;
+  projects:any;
+  noOfUsers:any;  
 
+  constructor( private userService: UserInfoService, private dialog:MatDialog ){
+    this.user = sessionStorage.getItem('currentUser')
+    this.user = JSON.parse(this.user)
+    
+  }
 }
---
+++ src/app/services/user-info.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserInfoService {
+  currentUser : any ;
+
+
+  constructor() { }
+
+  get(key: string, sessionType: string): any {
+    let data = sessionType === 'session' ? sessionStorage.getItem(key) : localStorage.getItem(key);
+    return data ? JSON.parse(data) : [];
+  }
+
+  store(value: any, key: string, sessionType: string): void {
+    sessionType === 'session' ? sessionStorage.setItem(key, JSON.stringify(value)) : localStorage.setItem(key, JSON.stringify(value));
+    console.log ('User stored')
+  }
+}
