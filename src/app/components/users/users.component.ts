e8848bd3a6cbebd6737f084a34f27d6a855f5253
NoxoloMnguni23 <kimberlymnguni@gmail.com>
2024-03-15 09:46:09 +0200

    users,table component

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent {
+
+
+
+  openDialog(){}
+  // constructor(private apiUserService : ApiService){}
+  // role : any ; 
+
+  // displayedColumns: string[] = ['firstName', 'lastName','gender', 'id','email','role','password'];
+  // dataSource = this.apiUserService.getUsers(Users);
+
+  // applyFilter(event: Event) {
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  //   if (this.dataSource.paginator) {
+  //     this.dataSource.paginator.firstPage();
+  //   }
+  // }
 
 }
