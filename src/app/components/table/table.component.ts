562f27b1d8cad4f4b7de6849722fe8abc44374bb
NoxoloMnguni23 <kimberlymnguni@gmail.com>
2024-03-18 10:21:37 +0200

    edit and delete add funtion

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -2,6 +2,9 @@ import { AfterViewInit, Component, ViewChild, Input, OnChanges, SimpleChanges, O
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { ApiService } from 'src/app/services/api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AddUserFormComponent } from '../add-user-form/add-user-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-table',
@@ -14,25 +17,23 @@ export class TableComponent implements OnChanges {
   users: any;
   isAdmin: boolean = false;
   isTeamMember: boolean = false;
-  displayedColumns: string[] = []; 
+  displayedColumns: string[] = [];
   displayedHeaders: string[] = [];
   dataSource!: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private snackBar: MatSnackBar, private dialog: MatDialog) { }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (this.tableData.title === 'Users') {
       this.isAdmin = true;
       // Fetch user data from API
-      this.apiService.genericGet('/get-users').subscribe((res: any) => {
-        this.dataSource = new MatTableDataSource<any>(res);
-      });
+      this.refreshUsers()
     } else if (this.tableData.title === 'Tasks') {
       this.isAdmin = false;
-      this.apiService.genericGet('/get-members-tasks').subscribe((res: any) => {
-        this.dataSource = new MatTableDataSource<any>(res);
-      });
+      // Fetch tasks data from API
+      this.refreshTasks();
     }
     if (changes['tableData']) {
       this.dataSource = new MatTableDataSource(this.tableData.dataSource);
@@ -54,5 +55,62 @@ export class TableComponent implements OnChanges {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  deleteRow(row: any) {
+    const confirmDelete = prompt('type delete to confirm');
+    if (confirmDelete?.toLowerCase() === 'delete') {
+      // If delete confirmed
+      if (this.tableData.title === 'Users') {
+        this.apiService.genericDelete(`/delete-user/${row.email}`)
+          .subscribe({
+            next: (res) => {
+              console.log("row parsed", res)
+              // Fetch user data from API
+              this.refreshUsers()
+              this.snackBar.open('User deleted successfully', 'Ok', { duration: 3000 });
+            },
+            error: (res) => {
+              this.snackBar.open('Error deleting user', 'Ok', { duration: 3000 });
+            },
+            complete: () => { }
+          })
+      } else {
+        // To edit for task
+        // this.apiService.genericDelete(`/delete-user/${row.email}`)
+        //   .subscribe({
+        //     next: (res) => {
+        //       console.log("row parsed", res)
+        //       // Fetch user data from API
+        //       this.refreshUsers()
+        //       this.snackBar.open('User deleted successfully', 'Ok', { duration: 3000 });
+        //     },
+        //     error: (res) => {
+        //       this.snackBar.open('Error deleting user', 'Ok', { duration: 3000 });
+        //     },
+        //     complete: () => { }
+        //   })
+      }
+
+    } else {
+      this.snackBar.open('Action cancelled', 'Ok', { duration: 3000 })
+    }
+  }
+  editRow(row: any) {
+    this.dialog.open(AddUserFormComponent, { data: row });
+    console.log("row", row)
+  }
+
+  refreshUsers(): void {
+    // Fetch user data from API
+    this.apiService.genericGet('/get-users').subscribe((res: any) => {
+      this.dataSource = new MatTableDataSource<any>(res);
+    });
+  }
+
+  refreshTasks(): void {
+    this.apiService.genericGet('/get-members-tasks').subscribe((res: any) => {
+      this.dataSource = new MatTableDataSource<any>(res);
+    });
   }
 }
