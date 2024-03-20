8361c21a937f24699f04d4122df013c28764055a
NoxoloMnguni23 <kimberlymnguni@gmail.com>
2024-03-20 10:00:37 +0200

    team memebers

--- src/app/components/team-members/team-members.component.ts
+++ src/app/components/team-members/team-members.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-team-members',
   templateUrl: './team-members.component.html',
   styleUrls: ['./team-members.component.scss']
 })
-export class TeamMembersComponent {
+export class TeamMembersComponent implements AfterViewInit {
+
+  user: any;
+  teamMembers: any = {}
+  teamMemberData: any;
+
+  ngAfterViewInit(): void {
+   
+  }
+  constructor(private api: ApiService) {
+    this.api.genericGet('/get-users').subscribe({
+      next: (res: any) => {
+        console.log("res",res);
+        this.teamMemberData = res.filter((user: any) => user.role === 'team member');
+        console.log("teamMemberData", this.teamMemberData)
+        this.teamMemberFunction();
+      }
+    })
+
+   
+    
+  }
+  teamMemberFunction(){
+
+    this.teamMembers =
+    {
+      title: 'Users',
+      dataSource: this.teamMemberData,
+      displayedColumns: ['firstName', 'lastName', 'gender', 'id', 'email', 'role'],
+      displayedHeaders: ['First Name', 'Last Name', 'Gender', 'ID', 'Email', 'Role']
+    }
+
+  }
+
 
 }
